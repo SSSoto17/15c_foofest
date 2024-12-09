8fbad5829d4bb5ec633d5b4db288add5fbeb0892
Sif Melkane <siha0004@stud.kea.dk>
2024-12-09 15:49:10 +0100

    SortByMenu created

---
+++ src/components/lineup/SortByMenu.jsx
@@ -0,0 +1,14 @@
+const SortByMenu = () => {
+  return (
+    <section className="border-2 border-border-global ">
+      <header className="p-4 border-b-2 border-b-border-global heading-4">Sort By</header>
+      <ul className="p-4">
+        <li>Alphabet</li>
+        <li>Genre</li>
+        <li>Cancelled</li>
+      </ul>
+    </section>
+  );
+};
+
+export default SortByMenu;
