0f97ab7d9c2ef5231cbf14469d924abaf1436733
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2024-12-09 12:29:07 +0100

    @apply text styles added to global css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,12 +14,6 @@ export default function Home() {
         <li className="bg-forest-900 w-20 h-20"></li>
         <li className="bg-forest-950 w-20 h-20"></li>
       </ul>
-      <h1>Heading</h1>
-      <h2>Heading</h2>
-      <h3>Heading</h3>
-      <h4>Heading</h4>
-      <h5>Heading</h5>
-      <h6>Heading</h6>
     </main>
   );
 }
