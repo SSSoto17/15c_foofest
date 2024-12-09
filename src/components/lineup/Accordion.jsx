2e628824b64778b9509a9cd2e798a1199e0d8254
Sif Melkane <siha0004@stud.kea.dk>
2024-12-09 15:28:29 +0100

    Accordion: Adding prop and adjusting border color

--- src/components/lineup/Accordion.jsx
+++ src/components/lineup/Accordion.jsx
@@ -1,10 +1,10 @@
 import { MdAdd } from "react-icons/md";
 
-const Accordion = ({ children }) => {
+const Accordion = ({ children, summary }) => {
   return (
-    <details className="group border-2 px-6 py-6">
+    <details className="group border-2 border-border-global px-6 py-6">
       <summary className="cursor-pointer list-none flex items-center justify-between heading-2">
-        Jotunheim
+        {summary}
         <MdAdd size="2rem" className="duration-300 ease-in-out group-open:rotate-45" />
       </summary>
       {children}
