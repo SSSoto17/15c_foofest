c5489bb2d5b42d1e428110522082cb15363463ba
Sif Melkane <siha0004@stud.kea.dk>
2024-12-10 09:45:53 +0100

    styling adjustments

--- src/components/lineup/ArtistCard.jsx
+++ src/components/lineup/ArtistCard.jsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import picture from "../../assets/tester/terminalist.jpg";
+import Link from "next/link";
 
 const ArtistCard = () => {
   return (
-    <li className="grid">
-      <Image src={picture} alt="Billede af band" className="grayscale row-start-1 col-start-1"></Image>
-      <h3 className="z-1 text-aztec-100 heading-2 px-6 py-1 row-start-1 col-start-1 self-end bg-gradient-to-t from-black">Terminalist</h3>
+    <li>
+      <Link href="/" className="grid aspect-square">
+        <Image src={picture} alt="Billede af band" className="grayscale row-start-1 col-start-1"></Image>
+        <h3 className="z-1 text-aztec-100 heading-2 px-6 py-1 row-start-1 col-start-1 self-end bg-gradient-to-t from-black">Terminalist</h3>
+      </Link>
     </li>
   );
 };
--- src/components/lineup/SortByMenu.jsx
+++ src/components/lineup/SortByMenu.jsx
@@ -1,6 +1,6 @@
 const SortByMenu = () => {
   return (
-    <section className="border-2 border-border-global ">
+    <section className="border-2 border-border-global self-start">
       <header className="p-4 border-b-2 border-b-border-global heading-4">Sort By</header>
       <ul className="p-4">
         <li>Alphabet</li>
