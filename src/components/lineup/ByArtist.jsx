cdae87e4e73a749c6d0f922d27d276e3469ffe5f
Sif Melkane <siha0004@stud.kea.dk>
2024-12-16 10:56:15 +0100

    Still working on genre filtering - but need to commit to switch branch

--- src/components/lineup/ByArtist.jsx
+++ src/components/lineup/ByArtist.jsx
@@ -1,16 +1,23 @@
+"use client";
+import { useState } from "react";
 import ArtistCard from "@/components/lineup/ArtistCard";
 import SortByMenu from "@/components/lineup/SortByMenu";
 
-const ByArtist = ({ data }) => {
-  const artists = data;
+const ByArtist = ({ artists }) => {
   const endpoint = process.env.FOO_FEST_API_URL;
+  const genres = Object.groupBy(artists, ({ genre }) => genre);
+  const genreNames = Object.keys(genres);
+
+  console.log("GENRES ARRAY", genres);
+  console.log("JAZZ", genres["Alternative Metal"]);
 
   return (
     <section className="grid grid-cols-4 gap-4">
-      <SortByMenu></SortByMenu>
+      <SortByMenu genreNames={genreNames}></SortByMenu>
+
       <ul className="grid grid-cols-subgrid col-start-2 col-span-3 gap-4">
-        {artists.map((artist) => (
-          <ArtistCard key={artist.slug} name={artist.name} slug={artist.slug} img={artist.logo.startsWith("https://") ? artist.logo : `${endpoint}/logos/${artist.logo}`}></ArtistCard>
+        {artists.map((artist, i) => (
+          <ArtistCard key={i} name={artist.name} slug={artist.slug} img={artist.logo.startsWith("https://") ? artist.logo : `${endpoint}/logos/${artist.logo}`}></ArtistCard>
         ))}
       </ul>
     </section>
--- src/components/lineup/SortByMenu.jsx
+++ src/components/lineup/SortByMenu.jsx
@@ -1,13 +1,27 @@
-const SortByMenu = () => {
+"use client";
+import { useState } from "react";
+import { MdOutlineCheck } from "react-icons/md";
+
+import { Checkbox } from "@headlessui/react";
+
+const SortByMenu = ({ genreNames }) => {
+  const [checked, setCheck] = useState(false);
+
   return (
-    <section className="border-2 border-border-global self-start">
-      <header className="p-4 border-b-2 border-b-border-global heading-4">Sort By</header>
-      <ul className="p-4">
-        <li>Alphabet</li>
-        <li>Genre</li>
-        <li>Cancelled</li>
-      </ul>
-    </section>
+    <form>
+      <details className="border-2 border-border-global self-start">
+        <summary className="p-4 border-b-2 border-b-border-global heading-4">Filter By Genre</summary>
+        <ul className="p-4">
+          {genreNames.map((genreName, i) => (
+            <li key={i}>
+              <Checkbox name={genreName} checked={checked} onChange={setCheck} className="border-2 border-aztec-600 rounded-sm data-checked:border-forest-600 data-checked:bg-forest-600 data-focus:outline-none">
+                <MdOutlineCheck className={`opacity-0 ${checked && "opacity-100"}`} />
+              </Checkbox>
+            </li>
+          ))}
+        </ul>
+      </details>
+    </form>
   );
 };
 
