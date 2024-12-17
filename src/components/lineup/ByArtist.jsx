bcfbb4ce53e7592b757a8b0766d4a74897df9dea
Sif Melkane <siha0004@stud.kea.dk>
2024-12-17 18:52:20 +0100

    Scroll to top button added

--- src/components/lineup/ByArtist.jsx
+++ src/components/lineup/ByArtist.jsx
@@ -1,6 +1,7 @@
 "use client";
 import ArtistCard from "@/components/lineup/ArtistCard";
 import SortByMenu from "@/components/lineup/SortByMenu";
+import ScrollToButton from "./ScrollToButton";
 
 const ByArtist = ({ artists, filters }) => {
   const endpoint = process.env.FOO_FEST_API_URL;
@@ -23,31 +24,34 @@ const ByArtist = ({ artists, filters }) => {
   // console.log("JAZZ", genres["Alternative Metal"]);
 
   return (
-    <section className="grid grid-cols-4 gap-4">
-      <SortByMenu genreNames={sortedGenreNames}></SortByMenu>
-      {filters ? (
-        filters.map((filter, i) => {
-          return (
-            <article className="grid grid-cols-subgrid col-start-2 col-span-3 gap-4 mb-8" key={i}>
-              <h2 className="heading-3 col-span-full">{filter}</h2>
-              <ul className="grid grid-cols-subgrid col-span-3 gap-4">
-                {artists
-                  .filter((artist) => artist.genre === filter)
-                  .map((artist, i) => (
-                    <ArtistCard key={i} name={artist.name} slug={artist.slug} img={artist.logo.startsWith("https://") ? artist.logo : `${endpoint}/logos/${artist.logo}`}></ArtistCard>
-                  ))}
-              </ul>
-            </article>
-          );
-        })
-      ) : (
-        <ul className="grid grid-cols-subgrid col-span-3 gap-4">
-          {artists.map((artist, i) => (
-            <ArtistCard key={i} name={artist.name} slug={artist.slug} img={artist.logo.startsWith("https://") ? artist.logo : `${endpoint}/logos/${artist.logo}`}></ArtistCard>
-          ))}
-        </ul>
-      )}
-    </section>
+    <>
+      <section className="grid grid-cols-4 gap-4">
+        <SortByMenu genreNames={sortedGenreNames}></SortByMenu>
+        {filters ? (
+          filters.map((filter, i) => {
+            return (
+              <article className="grid grid-cols-subgrid col-start-2 col-span-3 gap-4 mb-8" key={i}>
+                <h2 className="heading-3 col-span-full">{filter}</h2>
+                <ul className="grid grid-cols-subgrid col-span-3 gap-4">
+                  {artists
+                    .filter((artist) => artist.genre === filter)
+                    .map((artist, i) => (
+                      <ArtistCard key={i} name={artist.name} slug={artist.slug} img={artist.logo.startsWith("https://") ? artist.logo : `${endpoint}/logos/${artist.logo}`}></ArtistCard>
+                    ))}
+                </ul>
+              </article>
+            );
+          })
+        ) : (
+          <ul className="grid grid-cols-subgrid col-span-3 gap-4">
+            {artists.map((artist, i) => (
+              <ArtistCard key={i} name={artist.name} slug={artist.slug} img={artist.logo.startsWith("https://") ? artist.logo : `${endpoint}/logos/${artist.logo}`}></ArtistCard>
+            ))}
+          </ul>
+        )}
+      </section>
+      <ScrollToButton></ScrollToButton>
+    </>
   );
 };
 
