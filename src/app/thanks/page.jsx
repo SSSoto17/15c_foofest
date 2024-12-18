84c065fc16cf5423825355dbdb29ad98e0dd84e0
Sif Melkane <siha0004@stud.kea.dk>
2024-12-18 14:16:26 +0100

    setting up basics

---
+++ src/app/thanks/page.jsx
@@ -0,0 +1,13 @@
+export default function Thanks() {
+  return (
+    <main>
+      <section className="flow-space">
+        <h1 className="heading-1 text-balance">Wuhuu! You’re ready for FooFest</h1>
+        <p>You have succesfully purchased tickets for FooFest 2025. We’ll send you an e-mail with the tickets shortly. </p>
+        <p>Meanwhile check out the schedule!</p>
+
+        <div className="bg-forest-600 p-4 inline-block">BUTTON</div>
+      </section>
+    </main>
+  );
+}
