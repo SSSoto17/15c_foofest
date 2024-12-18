feef16b79090f2e1690c1c0cafcc04964c5e915a
Sif Melkane <siha0004@stud.kea.dk>
2024-12-18 15:01:36 +0100

    More styling and cards now links

--- src/app/(festival)/page.jsx
+++ src/app/(festival)/page.jsx
@@ -4,18 +4,18 @@ import hero from "@/assets/img/hero.png";
 export default function Home() {
   return (
     <main className="full-bleed">
-      <section className="grid grid-cols-subgrid col-start-1 col-span-full mb-12">
+      <section className="full-bleed mb-12">
         <article className="full-bleed row-start-1 row-span-4">
-          <Image className="col-start-1 col-span-full row-start-1" src={hero} alt="Image of FooFest participants. Photo by Roberto Rendon on Unsplash."></Image>
+          <Image className="full-bleed row-start-1" src={hero} alt="Image of FooFest participants. Photo by Roberto Rendon on Unsplash."></Image>
           <div className="col-start-2 row-start-1 self-center flow-space grid grid-cols-3">
-            <h1 className="heading-title text-balance col-span-full">Claim your ticket for FooFest</h1>
+            <h1 className="heading-title text-desk-3xl text-balance col-span-full">Claim your ticket for FooFest</h1>
             {/* <p className="text-balance text-desk-lg col-start-1 col-span-2">Enjoy the greatest rock music of all time and let your inner viking loose!</p> */}
           </div>
         </article>
         <footer className="col-start-2 row-start-4 row-span-2 grid grid-cols-3 gap-6">
           <div className="col-start-2 col-span-full grid grid-cols-subgrid">
-            <TicketCard type="Partout" price="799" variant="single"></TicketCard>
-            <TicketCard type="VIP" price="1299" variant="multiple"></TicketCard>
+            <TicketCard type="Partout" price="799" variant="single" description="Full festival access and immerse yourself in the energy of live rock music!"></TicketCard>
+            <TicketCard type="VIP" price="1299" variant="multiple" description="Front-row access, exclusive lounges, and premium perks. Experience the festival like never before!"></TicketCard>
           </div>
         </footer>
       </section>
--- src/components/TicketCard.jsx
+++ src/components/TicketCard.jsx
@@ -1,15 +1,19 @@
+import Link from "next/link";
+
 const TicketCard = ({ type, description, price, variant }) => {
   const variants = {
-    single: "bg-[url('../assets/svg/diamond.svg')] bg-[right_0.5rem_bottom_-3.5rem]",
+    single: "bg-[url('../assets/svg/diamond.svg')] bg-[right_-0.5rem_bottom_-3.5rem]",
     multiple: "bg-[url('../assets/svg/diamonds.svg')] bg-[right_-5rem_bottom_-3.5rem]",
   };
 
   return (
-    <article className={`flow-space px-8 pt-6 pb-2 bg-aztec-900 ${variants[variant]} bg-no-repeat 	`}>
-      <h2 className="heading-2 text-desk-xl">{type} ticket</h2>
-      <p className="text-balance">Lorem ipsum ipsa expedita facilis eius fugiat hic nostrum dicta, suscipit ad odio. Eum?</p>
-      <p className="text-right heading-2 text-desk-lg">{price} ,-</p>
-    </article>
+    <Link href="/booking">
+      <article className={`px-8 pt-6 pb-2 bg-aztec-950 ${variants[variant]} bg-no-repeat hover:scale-101	transition-transform	`}>
+        <h2 className="heading-2 text-desk-lg">{type} ticket</h2>
+        <p className="">{description}</p>
+        <p className="text-right heading-2 text-2xl">{price} ,-</p>
+      </article>
+    </Link>
   );
 };
 
