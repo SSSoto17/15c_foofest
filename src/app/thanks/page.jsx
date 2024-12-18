73b766ba5dccee8b887aa8ffc059acca6d8b649a
Sif Melkane <siha0004@stud.kea.dk>
2024-12-18 16:33:53 +0100

    Setting up for order session response

--- src/app/thanks/page.jsx
+++ src/app/thanks/page.jsx
@@ -1,13 +1,20 @@
+import Image from "next/image";
+import succes from "@/assets/img/succes.png";
+
 export default function Thanks() {
   return (
     <main>
-      <section className="flow-space">
-        <h1 className="heading-1 text-balance">Wuhuu! You’re ready for FooFest</h1>
-        <p>You have succesfully purchased tickets for FooFest 2025. We’ll send you an e-mail with the tickets shortly. </p>
-        <p>Meanwhile check out the schedule!</p>
-
-        <div className="bg-forest-600 p-4 inline-block">BUTTON</div>
-      </section>
+      <article className="grid grid-cols-10 h-full">
+        <Image className="row-start-1 col-start-6 col-span-full object-cover h-full" src={succes} alt="Image from Foo Fest. Photo by Roberto Rendon on Unsplash"></Image>
+        <section className="flow-space row-start-1 col-start-1 col-span-7 py-20">
+          <h1 className="heading-1 uppercase">Wuhuu! You’re ready for FooFest</h1>
+          <p className="body-copy">
+            You have succesfully purchased tickets for FooFest 2025. <br /> We’ll send you an e-mail with the tickets shortly.{" "}
+          </p>
+          <p className="body-copy">Meanwhile check out the schedule!</p>
+          <div className="bg-forest-600 p-4 inline-block">BUTTON</div>
+        </section>
+      </article>
     </main>
   );
 }
