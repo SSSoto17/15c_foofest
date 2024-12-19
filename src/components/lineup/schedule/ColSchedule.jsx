7813d9dd2d6cd644155d6590c26757f8d4646738
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2024-12-20 00:26:15 +0100

    .

--- src/components/lineup/schedule/ColSchedule.jsx
+++ src/components/lineup/schedule/ColSchedule.jsx
@@ -9,8 +9,12 @@ const ColSchedule = ({ data }) => {
             <TableCell></TableCell>
           ) : act.cancelled ? (
             <TableCell>
-              <p className="col-start-1 text-res-sm sm:text-res-base">{act.act}</p>
-              <p className="col-start-1 opacity-75 -rotate-12 text-res-xs sm:text-res-sm uppercase border-2 border-gold-600 text-gold-600 inline-block px-1 sm:px-2 sm:py-0.5">Cancelled</p>
+              <p className="col-start-1 text-res-sm sm:text-res-base">
+                {act.act}
+              </p>
+              <p className="col-start-1 opacity-75 -rotate-12 text-res-xs sm:text-res-sm uppercase border-2 border-gold-600 text-gold-600 inline-block px-1 sm:px-2 sm:py-0.5">
+                Cancelled
+              </p>
             </TableCell>
           ) : (
             <TableCell>
