c7bdc54ae0c15276e068a032ac708ebb3858bf5c
Sif Melkane <siha0004@stud.kea.dk>
2024-12-19 15:02:58 +0100

    card grid responsive

--- src/components/lineup/ByArtist.jsx
+++ src/components/lineup/ByArtist.jsx
@@ -1,32 +1,14 @@
 "use client";
 import ArtistCard from "@/components/lineup/ArtistCard";
-import SortByMenu from "@/components/lineup/SortByMenu";
 import ScrollToButton from "./ScrollToButton";
 
-const ByArtist = ({ artists, filters }) => {
+const ByArtist = ({ artists }) => {
   const endpoint = process.env.FOO_FEST_API_URL;
-  const genres = Object.groupBy(artists, ({ genre }) => genre);
-  const genreNames = Object.keys(genres);
-  const sortedGenreNames = genreNames.toSorted();
   console.log("ARTIST:", artists);
-
-  // const filteredArtists = filters.map((genre) => {
-  //   return artists.filter((artist) => artist.genre === genre);
-  // });
-
-  // const displayArtists = filters ? filteredArtists : artists;
-
-  // console.log("ARTISTS", artists);
-  // console.log("FILTERS", filteredArtists);
-  // console.log("GENRES", genres);
-  // console.log(artists[1].genre);
-
-  // console.log("GENRES ARRAY", genres);
-  // console.log("JAZZ", genres["Alternative Metal"]);
 
   return (
     <section>
-      <ul className="grid grid-cols-4 gap-4">
+      <ul className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-4">
         {artists.map((artist, i) => (
           // console.log("ARTIST:", artist.slug)
           <ArtistCard key={i} name={artist.name} slug={artist.slug} img={artist.logo.startsWith("https://") ? artist.logo : `${endpoint}/logos/${artist.logo}`}></ArtistCard>
