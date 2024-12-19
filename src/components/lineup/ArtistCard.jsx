ca13e570e9eaa26edcfcd173ebfe4f0019187d0e
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2024-12-19 18:44:47 +0100

    testing

--- src/components/lineup/ArtistCard.jsx
+++ src/components/lineup/ArtistCard.jsx
@@ -8,8 +8,16 @@ const ArtistCard = ({ name, slug, img }) => {
   return (
     <li>
       <Link href={`artist/${slug}`} className="grid aspect-square">
-        <Image src={picture} width="400" height="400" alt={`Image of ${name}`} className="grayscale row-start-1 col-start-1"></Image>
-        <h3 className="z-1 text-aztec-100 heading-2 px-6 py-1 row-start-1 col-start-1 self-end bg-gradient-to-t from-black">{name}</h3>
+        <Image
+          src={img}
+          width="400"
+          height="400"
+          alt={`Image of ${name}`}
+          className="grayscale row-start-1 col-start-1"
+        ></Image>
+        <h3 className="z-1 text-aztec-100 heading-2 px-6 py-1 row-start-1 col-start-1 self-end bg-gradient-to-t from-black">
+          {name}
+        </h3>
       </Link>
     </li>
   );
